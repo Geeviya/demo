3aa86adfdd291c0d33b2383145bbfeb5790bf21b
Geeviya <geeviya8989@gmail.com>
2025-08-07 12:12:04 +0530

    3rd data

--- muthayammal/src/main/java/gitconnection/nkl.java
+++ muthayammal/src/main/java/gitconnection/nkl.java
@@ -3,8 +3,8 @@ package gitconnection;
 public class nkl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated me;thod stub
+		System.out.println("hello");
 	}
 
 }
---
+++ muthayammal/src/main/java/gitconnection/string.java
@@ -0,0 +1,15 @@
+package gitconnection;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="apple";
+		String b="orange";
+		String c=a+b;
+		System.out.println(c);
+		String reversed=new StringBuilder(c).reverse().toString();
+		System.out.println(reversed);
+	}
+
+}
