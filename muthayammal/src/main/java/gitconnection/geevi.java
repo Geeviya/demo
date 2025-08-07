869a6761ee830cc0d8307ae632153998c0e3b3e0
Geeviya <geeviya8989@gmail.com>
2025-08-07 10:28:33 +0530

    pushing the data to github

---
+++ muthayammal/src/main/java/gitconnection/geevi.java
@@ -0,0 +1,18 @@
+package gitconnection;
+
+import java.util.Scanner;
+
+public class geevi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n =s.nextInt(),f=1;
+		for(int i=1;i<n;i++) {
+			f=f*1;
+		}
+		System.out.println(f);
+		
+	}
+
+}
